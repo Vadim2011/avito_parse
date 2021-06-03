a69167970ef6f5289afc04649341cea6567eddf2
vadim2011 <rabota52933@mail.ru>
2021-06-04 01:04:57 +0800

    init develop commit

---
+++ main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf8 -*-
+
+# ---------------------------------------------
+# Program by Vadim 2011
+#
+#
+# Version    Date            Info
+# 0.1        03.06.2021      Parser Avito
+# ---------------------------------------------
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    pass
+
